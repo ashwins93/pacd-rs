003d80f0cab0e50f3f18d0dba6f88d233c7f7f09
Ashwin <aswinjeganath@gmail.com>
2023-01-31 16:19:34 +0530

    extend CLI for packer module

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,27 @@
-use clap::Parser;
+use clap::{Args, Parser};
 use log::error;
 use notify::RecursiveMode;
 use notify_debouncer_mini::{new_debouncer, DebouncedEventKind};
 use std::{error::Error, time::Duration};
 
-use pacd::SiteGenerator;
+use pacd::{
+    packer::{Packer, PackerConfig},
+    Config, SiteGenerator,
+};
+
+#[derive(Parser, Debug, Clone)]
+#[command(name = "pacd")]
+enum PacdCli {
+    #[command(name = "build")]
+    Builder(BuildArgs),
+    #[command(name = "pack")]
+    Packer(PackerArgs),
+}
 
 /// A static site generator based on shopify liquid
-#[derive(Debug, Parser, Clone)]
+#[derive(Debug, Args, Clone)]
 #[command(version, author, about, long_about = None)]
-struct CliArgs {
+struct BuildArgs {
     /// The path for output
     #[arg(short, long, default_value = "./build")]
     output_dir: std::path::PathBuf,
@@ -26,34 +38,66 @@ struct CliArgs {
     site_dir: std::path::PathBuf,
 }
 
+/// Pack your template files into an archive
+#[derive(Debug, Args, Clone)]
+#[command(version, author, about, long_about = None)]
+struct PackerArgs {
+    /// The path for output
+    #[arg(short, long, default_value = "./build.tar.gz")]
+    output_path: std::path::PathBuf,
+
+    /// Path to the source files
+    template_dir: std::path::PathBuf,
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     env_logger::init();
 
-    let args = CliArgs::parse();
+    let cmd = PacdCli::parse();
 
-    if args.watch {
-        println!("Watching for file changes in {}", args.site_dir.display());
-        match SiteGenerator::build(&args.site_dir, &args.output_dir, &args.data_path)
-            .and_then(|mut s| s.generate())
-        {
-            Ok(_) => println!("Build successful"),
-            Err(e) => {
-                error!("Build failed: {:#?}", e);
+    match cmd {
+        PacdCli::Builder(args) => {
+            let config = Config {
+                src_path: &args.site_dir,
+                dest_path: &args.output_dir,
+                data_path: &args.data_path,
+            };
+
+            if args.watch {
+                println!("Watching for file changes in {}", args.site_dir.display());
+
+                match SiteGenerator::build(config).and_then(|mut s| s.generate()) {
+                    Ok(_) => println!("Build successful"),
+                    Err(e) => {
+                        error!("Build failed: {:#?}", e);
+                    }
+                }
+                watch_changes(&args.site_dir, || {
+                    let config = Config {
+                        src_path: &args.site_dir,
+                        dest_path: &args.output_dir,
+                        data_path: &args.data_path,
+                    };
+                    match SiteGenerator::build(config).and_then(|mut s| s.generate()) {
+                        Ok(_) => println!("Build successful"),
+                        Err(e) => {
+                            error!("Build failed: {:#?}", e);
+                        }
+                    }
+                    Ok(())
+                })?;
+            } else {
+                SiteGenerator::build(config)?.generate()?;
             }
         }
-        watch_changes(&args.site_dir, || {
-            match SiteGenerator::build(&args.site_dir, &args.output_dir, &args.data_path)
-                .and_then(|mut s| s.generate())
-            {
-                Ok(_) => println!("Build successful"),
-                Err(e) => {
-                    error!("Build failed: {:#?}", e);
-                }
-            }
-            Ok(())
-        })?;
-    } else {
-        SiteGenerator::build(&args.site_dir, &args.output_dir, &args.data_path)?.generate()?;
+        PacdCli::Packer(args) => {
+            let packer = Packer::new(PackerConfig {
+                src_path: &args.template_dir,
+                dest_path: &args.output_path,
+            });
+
+            packer.pack()?;
+        }
     }
 
     Ok(())
